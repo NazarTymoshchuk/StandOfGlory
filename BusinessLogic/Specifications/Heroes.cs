7ae7eee29e46126028a16540d6258231798c1aa6
Nazar <Timo_qm01@student.itstep.org>
2023-03-17 16:51:43 +0200

    Add Filters

--- BusinessLogic/Specifications/Heroes.cs
+++ BusinessLogic/Specifications/Heroes.cs
@@ -23,5 +23,35 @@ namespace BusinessLogic.Specifications
                     .Include(x => x.Card);
             }
         }
+
+        public class FilterByCity : Specification<Hero>
+        {
+            public FilterByCity(string city)
+            {
+                Query
+                    .Include(x => x.Card)
+                    .Where(x => x.City.Name == city);
+            }
+        }
+
+        public class FilterByBattalion : Specification<Hero>
+        {
+            public FilterByBattalion(string battalion)
+            {
+                Query
+                    .Include(x => x.Card)
+                    .Where(x => x.Battalion.Name == battalion);
+            }
+        }
+
+        public class FilterByName : Specification<Hero>
+        {
+            public FilterByName(string name)
+            {
+                Query
+                    .Include(x => x.Card)
+                    .Where(x => x.Name.Contains(name));
+            }
+        }
     }
 }
